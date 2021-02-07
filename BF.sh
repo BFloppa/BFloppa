c8b65f569130406436ec6846c4d075a33821a0cb
BFloppa <77999174+BFloppa@users.noreply.github.com>
2021-02-07 00:44:41 -0300

    Update BF.sh

--- BF.sh
+++ BF.sh
@@ -1,19 +1,29 @@
 #!/bin/bash 
-
-
+#
 #EN CREACION BETA
+#
+#Colores
+#
+verde='\033[32m'
+blanco='\033[37m'
+rojo='\033[31m'
+azul='\033[34m'
+negro='\033[0;30m'
+rosa='\033[38;5;207m'
+amarillo='\033[33m'
+morado='\033[35m'
+cian='\033[1;36m'
+magenta='\033[1;35m'
 
 
 
-echo   "    ▀█████████▄     ▄████████     ███      ▄██████▄   ▄██████▄   ▄█       
-             ███    ███   ███    ███ ▀█████████▄  ███    ███ ███    ███ ███       
-             ███    ███   ███    █▀      ▀███▀▀██ ███    ███ ███    ███ ███       
-            ▄███▄▄▄██▀   ▄███▄▄▄          ███   ▀ ███    ███ ███    ███ ███       
-            ▀▀███▀▀▀██▄  ▀▀███▀▀▀         ███     ███    ███ ███    ███ ███       
-              ███    ██▄   ███            ███     ███    ███ ███    ███ ███       
-              ███    ███   ███            ███     ███    ███ ███    ███ ███▌    ▄ 
-             ▄█████████▀   ███           ▄████▀    ▀██████▀   ▀██████▀  █████▄▄██ 
-                                                            ▀         "
+
+echo -"       ██████╗ ███████╗ ████████╗ ██████╗  ██████╗ ██╗     
+        ██╔══██╗██╔════╝ ╚══██╔══╝██╔═══██╗██╔═══██╗██║     
+        ██████╔╝█████╗█████╗██║   ██║   ██║██║   ██║██║     
+        ██╔══██╗██╔══╝╚════╝██║   ██║   ██║██║   ██║██║     
+        ██████╔╝██║         ██║   ╚██████╔╝╚██████╔╝███████╗
+        ╚═════╝ ╚═╝         ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝"
 echo "
 ✩｡:*•.─────  ❁ BF-TOOL ❁  ─────.•*:｡✩ "
 
@@ -29,11 +39,10 @@ echo "
 
 #BIENVENIDO
 
-coded=CodedBy
-echo $coded
-coded=https://github.com/BFloppa/ 
-echo $coded
-echo "https://github.com/wedelf5254"
+echo -e "\e[31mCoded By\e[0m"
+
+echo -e "https://github.com/BFloppa"
+echo -e "https://github.com/wedelf5254\ "
 
 echo "Comandos: SpamSmS - soporte - ultratool"
 
@@ -49,6 +58,5 @@ echo " https://www.instagram.com/nolesabesalshitpost/ " ;;
 ultratool)
 echo " En creacion aun... " ;;
 *)
-echo " ERROR: Herramienta Incorrecta. Ingrese de nuevo. " ;;
+echo -e "\e[101mERROR: Herramienta Incorrecta. Ingrese de nuevo." ;;
 esac
-
